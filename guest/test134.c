11392536a077d6e461cc59ac99011961384d5f83
Nobuhiro MIKI <nob@bobuhiro11.net>
2018-04-10 22:38:08 +0900

    fix

--- guest/test134.c
+++ guest/test134.c
@@ -12,8 +12,24 @@ int fib(int n) {
     if (n < 2) {
         return 1;
     } else {
-        return fib(n) + fib(n-2);
+        return fib(n-1) + fib(n-2);
     }
+}
+
+int sum1(int n) {
+    if (n <= 0){
+        return 0;
+    } else {
+        return n + sum1(n-1);
+    }
+}
+
+int sum2(int n) {
+    int res=0;
+    while (n > 0) {
+        res += n--;
+    }
+    return res;
 }
 
 int main(void) {
@@ -22,10 +38,12 @@ int main(void) {
     res += f(5) - 0;
     res += fib(0) - 1;
     res += fib(1) - 1;
-    // res += fib(2) - 2;
+    res += fib(2) - 2;
     // res += fib(3) - 3;
     // res += fib(4) - 5;
     // res += fib(5) - 8;
+ 
+    res += sum1(10) + sum2(10) - 55 - 55;
 
     return res;
 }
