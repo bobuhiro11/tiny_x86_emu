15350ebdea6f26b3221dcbb4ab153a2765188401
Nobuhiro MIKI <nob@bobuhiro11.net>
2018-08-23 23:28:57 +0900

    fix

--- pic.go
+++ pic.go
@@ -1,10 +1,24 @@
 package main
 
-// Refs: http://oswiki.osask.jp/?%28PIC%298259A
-type PIC8259A struct {
-	commandReg uint8 // Command Register (Write Only)
-	statusReg  uint8 // Status Register (Read Only)
-	IRR        uint8 // Interrupt Request Register
-	ISR        uint8 // In-Service Register
-	IMR        uint8 // Interrupt Mask Register
+// Interrupts on xv6が詳しい
+
+// PIC(8259)は、IOAPIC（またはcpu0のlocal APIC）に接続されている
+// xv6では、レガシーデバイス（PS/2 keyboard, IDE, COM0）のために、PIC->IOAPICの順で割り込み設定をする
+
+// in CPU
+// - Select Interrupt Vector Number
+// - End of Interrupt
+// - Timer, Thermo sensor, Performance counter
+// - API IDs are unique in CPUs.
+type LocalAPIC struct {
+	IRR uint8 // Interrupt Request Register: CPUが未処理のベクタ番号にビットが立つ
+	ISR uint8 // In-Service Register：次に割り込む候補、EOIへ書き込まれると、IRR->ISRへビットが更新
+	IMR uint8 // Interrupt Mask Register, the bit is 0 only when the IRQ is enabled.
+}
+
+// at ICH (South Bridge)
+type IOAPIC struct {
+}
+
+type PIC struct {
 }
